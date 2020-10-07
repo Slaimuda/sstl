c3a66cd70b41661e41401ccb5f6837a28b6e9f43
DKZed <DKZed@users.noreply.github.com>
2020-10-07 09:54:27 +0000

    deploy: DKZed/ectl_repo@8a602ab11a79585c6a80456d5de00d4c133094c5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sstl/precache-manifest.c75c0a507509107cbcedce3fd406f94f.js"
+  "/ectl/precache-manifest.6c0b2834e0a12f5f28c6b3a1a0dd3e0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sstl/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ectl/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
