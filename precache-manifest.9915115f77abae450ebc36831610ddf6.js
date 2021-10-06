648b61f671cfbb59df2c4e1712993f6cdbbbf583
thebombzen <thebombzen@users.noreply.github.com>
2021-10-06 07:13:08 +0000

    deploy: DKZed/ectl_repo@1ee210faaf2c4ca47d198ce0033d39831c391825

--- precache-manifest.9915115f77abae450ebc36831610ddf6.js
+++ precache-manifest.9915115f77abae450ebc36831610ddf6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e8cc3e0954fb8b0834db0d7bd20436fa",
+    "revision": "472edadaad3d8922073cc3ff2fa825b8",
     "url": "/sstl/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sstl/static/css/2.01b883ae.chunk.css"
   },
   {
-    "revision": "4054d4fce905a16f1b17",
+    "revision": "9ccddc7878ef889721f4",
     "url": "/sstl/static/css/main.fc5be44d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sstl/static/js/2.38da7046.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4054d4fce905a16f1b17",
-    "url": "/sstl/static/js/main.c9da96ea.chunk.js"
+    "revision": "9ccddc7878ef889721f4",
+    "url": "/sstl/static/js/main.4c423eec.chunk.js"
   },
   {
     "revision": "da47a9e580359569ad2d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sstl/precache-manifest.4e3949c4bc0a5ecf12e118dc8c135d20.js"
+  "/sstl/precache-manifest.9915115f77abae450ebc36831610ddf6.js"
 );
 
 self.addEventListener('message', (event) => {
